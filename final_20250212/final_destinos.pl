9a613de089d2227aa6799fd8dc73095826af0693
francoschiavoni <francoschiavo7@gmail.com>
2025-05-09 08:56:56 -0300

    final destinos

---
+++ final_20250212/final_destinos.pl
@@ -0,0 +1,55 @@
+:-dynamic(destinosTuristicos/4).
+
+abrir:- retractall(destinosTuristicos(_,_,_,_)), consult('./bd_destinos.txt').
+
+menu:-abrir,
+      writeln('1 - Mostrar destinos segun parametros'),
+      writeln('2 - Mostrar destinos segun lista de parametros'),
+      writeln('0 - Salir'),
+      read(Opc),
+      Opc\=0,
+      opcion(Opc),
+      menu.
+menu:- writeln('Fin').
+
+pertenece(X,[X|_]).
+pertenece(X,[_|T]):- pertenece(X,T).
+
+mostrar_destinos(C,A,MaxDis,Cant):- destinosTuristicos(Nombre, Distancia, Actividades, Comidas),
+                               retract(destinosTuristicos(Nombre, Distancia, Actividades, Comidas)),
+                               pertenece(A,Actividades),
+                               pertenece(C,Comidas),
+                               Distancia < MaxDis,
+                               writeln(Nombre),
+                               mostrar_destinos(C,A,MaxDis,CantAnt),
+                               Cant is CantAnt + 1.
+mostrar_destinos(_,_,_,0).
+
+leer([H|T]):- read(H), H\=[], leer(T).
+leer([]).
+
+todas_pertenecen([],_).
+todas_pertenecen([H|T],Actividades):- pertenece(H,Actividades),
+                                      todas_pertenecen(T,Actividades).
+
+destinos_act(ListaAct, Cant):- destinosTuristicos(Nombre,_,Actividades,_),
+                               retract(destinosTuristicos(Nombre,_,Actividades,_)),
+                               todas_pertenecen(ListaAct, Actividades),
+                               writeln(Nombre),
+                               destinos_act(ListaAct, CantAnt),
+                               Cant is CantAnt +1.
+destinos_act(_,0).
+
+
+opcion(1):- writeln('Ingrese comida: '),read(C),
+            writeln('Ingrese actividad: '),read(A),
+            writeln('Ingrese distancia max: '),read(MaxDis),
+            mostrar_destinos(C,A,MaxDis, Cant),
+            Cant \= 0.
+opcion(1):- writeln('No se encontraron destinos').
+
+
+opcion(2):- writeln('Ingrese lista de actividades: '), leer(ListaAct),
+            destinos_act(ListaAct,Cant),
+            Cant \=0.
+opcion(2):- writeln('No se encontraron destinos').
