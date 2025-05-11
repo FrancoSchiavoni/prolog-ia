2d00b7ae42058763d87871482231861e6d594c34
Franco <francoschiavo7@gmail.com>
2025-05-11 10:03:03 -0300

    Fix destinos

--- final_20250212/final_destinos.pl
+++ final_20250212/final_destinos.pl
@@ -15,15 +15,13 @@ menu:- writeln('Fin').
 pertenece(X,[X|_]).
 pertenece(X,[_|T]):- pertenece(X,T).
 
-mostrar_destinos(C,A,MaxDis,Cant):- destinosTuristicos(Nombre, Distancia, Actividades, Comidas),
-                               retract(destinosTuristicos(Nombre, Distancia, Actividades, Comidas)),
+mostrar_destinos(C,A,MaxDis,[H|T]):- destinosTuristicos(H, Distancia, Actividades, Comidas),
+                               retract(destinosTuristicos(H, Distancia, Actividades, Comidas)),
                                pertenece(A,Actividades),
                                pertenece(C,Comidas),
                                Distancia < MaxDis,
-                               writeln(Nombre),
-                               mostrar_destinos(C,A,MaxDis,CantAnt),
-                               Cant is CantAnt + 1.
-mostrar_destinos(_,_,_,0).
+                               mostrar_destinos(C,A,MaxDis,T).
+mostrar_destinos(_,_,_,[]).
 
 leer([H|T]):- read(H), H\=[], leer(T).
 leer([]).
@@ -32,24 +30,25 @@ todas_pertenecen([],_).
 todas_pertenecen([H|T],Actividades):- pertenece(H,Actividades),
                                       todas_pertenecen(T,Actividades).
 
-destinos_act(ListaAct, Cant):- destinosTuristicos(Nombre,_,Actividades,_),
-                               retract(destinosTuristicos(Nombre,_,Actividades,_)),
+destinos_act(ListaAct, [H|T]):- destinosTuristicos(H,_,Actividades,_),
+                               retract(destinosTuristicos(H,_,Actividades,_)),
                                todas_pertenecen(ListaAct, Actividades),
-                               writeln(Nombre),
-                               destinos_act(ListaAct, CantAnt),
-                               Cant is CantAnt +1.
-destinos_act(_,0).
+                               destinos_act(ListaAct, T).
+destinos_act(_,[]).
 
 
 opcion(1):- writeln('Ingrese comida: '),read(C),
             writeln('Ingrese actividad: '),read(A),
             writeln('Ingrese distancia max: '),read(MaxDis),
-            mostrar_destinos(C,A,MaxDis, Cant),
-            Cant \= 0.
+            mostrar_destinos(C,A,MaxDis,Destinos),
+            Destinos\=[],
+            writeln(Destinos).
 opcion(1):- writeln('No se encontraron destinos').
 
 
+
 opcion(2):- writeln('Ingrese lista de actividades: '), leer(ListaAct),
-            destinos_act(ListaAct,Cant),
-            Cant \=0.
+            destinos_act(ListaAct,Destinos),
+            Destinos \= [],
+            writeln(Destinos).
 opcion(2):- writeln('No se encontraron destinos').
